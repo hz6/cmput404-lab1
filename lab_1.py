44c9759222681dc5d7b0207afe7882ebdc929ffa
hz6 <hz6@ualberta.ca>
2021-01-15 11:54:44 +0800

    modify

--- lab_1.py
+++ lab_1.py
@@ -1,9 +1,12 @@
 import requests as req
 
+print("The version of requests library:\n", req.__version__)
+pg = req.get("http://google.com")
+
 
 def main():
-    print(req.get("http://google.com"))
-    print(req.__version__)
+    print("Page Content:\n", pg.content)
+    print("Status Code:\n", pg.status_code)
 
 
 main()
